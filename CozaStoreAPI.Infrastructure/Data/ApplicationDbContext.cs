6712b59fb0351ee3ee0ef513655afc0eeb12544c
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-09-21 19:40:41 +0300

    creat DbContext. Set type of user id to Guid

---
+++ CozaStoreAPI.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CozaStoreAPI.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CozaStoreAPI.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser, IdentityRole<Guid>, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ CozaStoreAPI.Infrastructure/Data/Models/AppUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static CozaStoreAPI.Infrastructure.DataConstants.AppUser;
+
+namespace CozaStoreAPI.Infrastructure.Data.Models
+{
+    public class AppUser : IdentityUser<Guid>
+    {
+
+        [Comment("First Name")]
+        [MaxLength(FirstNameMaxLength)]
+        public string? FirstName { get; set; }
+
+        [Comment("Last Name")]
+        [MaxLength(LastNameMaxLength)]
+        public string? LastName { get; set; }
+
+    }
+}
