da05f6522a6ee1018a59d7eca3d9a475ccefc641
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-09-22 16:10:56 +0300

    Create Products Controler

---
+++ CozaStoreAPI/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CozaStoreAPI.Controllers
+{
+    [Authorize]
+    //[AutoValidateAntiforgeryToken]
+    public class BaseController : Controller
+    {
+    }
+}
