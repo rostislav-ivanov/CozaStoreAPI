32ad08e8a744c6becd84226058a9bc2d5e0dda95
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-09-29 18:32:36 +0300

    expose Swagger

--- CozaStoreAPI/Program.cs
+++ CozaStoreAPI/Program.cs
@@ -15,11 +15,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("AllowSpecificOrigins");
 app.UseHttpsRedirection();
